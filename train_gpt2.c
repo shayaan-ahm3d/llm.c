299212a79ccfb2a73adb8690105e032ba02d079c
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2026-05-12 07:50:44 +0100

    for what

--- train_gpt2.c
+++ train_gpt2.c
@@ -34,41 +34,7 @@ There will be other versions of this code that specialize it and make it fast.
 // ----------------------------------------------------------------------------
 // all the individual layers' forward and backward passes
 // B = batch_size, T = sequence_length, C = channels, V = vocab_size
-/*
-void dequantize(QuantizedTensor *qx, float* x, int n) {
-    for (int i = 0; i < n; i++) {
-        x[i] = qx->q[i] * qx->s[i / GS];
-    }
-}
 
-void quantize(QuantizedTensor *qx, float* x, int n) {
-    int num_groups = n / GS;
-    float Q_MAX = 127.0f;
-
-    for (int group = 0; group < num_groups; group++) {
-
-        // find the max absolute value in the current group
-        float wmax = 0.0;
-        for (int i = 0; i < GS; i++) {
-            float val = fabs(x[group * GS + i]);
-            if (val > wmax) {
-                wmax = val;
-            }
-        }
-
-        // calculate and write the scaling factor
-        float scale = wmax / Q_MAX;
-        qx->s[group] = scale;
-
-        // calculate and write the quantized values
-        for (int i = 0; i < GS; i++) {
-            float quant_value = x[group * GS + i] / scale; // scale
-            int8_t quantized = (int8_t) round(quant_value); // round and clamp
-            qx->q[group * GS + i] = quantized;
-        }
-    }
-}
-*/
 void encoder_forward(
 	float* output,
     const int* tokenIdsAtPosition,
